7afd8c2eda512c77cac7e7b61599433f00b8b6ac
Kristi Green <kngreen40@gmail.com>
2017-06-04 16:04:23 -0600

    Add styling fix image path

--- lib/components/DayCard.jsx
+++ lib/components/DayCard.jsx
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
-
+import iconKeys from './icon-keys';
+import $ from 'jquery';
 
 const DayCard = (props) => {
   return (
     <div className="ten-day-weather-card">
       <p className="ten-day-items">{props.day}</p>
       <div className="icon-card">
-        <p className="ten-day-items"><img alt="weather icon" src={props.icon}/></p>
+        <p className="ten-day-items"><img className="day-icons" alt="weather icon" src={`./lib/images/${iconKeys[props.icon]}`}/></p>
         <p className="ten-day-items">High: {props.hi}</p>
         <p className="ten-day-items">Low: {props.low}</p>
       </div>
