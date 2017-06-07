67926857663f8341663e41c3a68ae676cf2fdd2a
Kristi Green <kngreen40@gmail.com>
2017-06-07 08:53:29 -0600

    Add tests for sevenhour display

--- test/SevenHourDisplay.test.js
+++ test/SevenHourDisplay.test.js
@@ -4,5 +4,37 @@ import SevenHourDisplay from '../lib/components/SevenHourDisplay.jsx';
 
 describe('SevenHourDisplay', () => {
   it.skip('Should render', () => {
+    const component = shallow(<SevenHourDisplay time="12am" temp="85 F" icon="./lib/images/weather.svg" />);
+
+    expect(component.contains(<p className="seven-hour-display">
+      12am
+    </p>)).toEqual(true);
+
+    expect(component.contains(<p className="seven-hour-display">
+      85 F℉
+    </p>)).toEqual(true);
+
+    expect(component.contains(<p className="seven-hour-display">
+      <img className="hour-icons" alt="weather icon" src="./lib/images/undefined" />
+    </p>)).toEqual(true);
+
+    expect(component.contains('85 F')).toEqual(true);
+
+    expect(component.contains(<div className="weather-card">
+      <p className="hour-items">
+        12am
+      </p>
+      <div className="icon-card">
+        <p className="hour-items">
+          <img className="hour-icons" alt="weather icon" src="./lib/images/undefined" />
+        </p>
+        <p className="hour-temp-text">
+        Temp
+        </p>
+        <p className="hour-temp">
+        85 F℉
+        </p>
+      </div>
+    </div>)).toEqual(true);
   });
 });
